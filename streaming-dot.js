a186cb9d9130be5081c04663f74ec7f4093dfa9f
Surma <surma@surma.link>
2016-11-13 20:57:56 -0800

    Add support for evaluate

--- streaming-dot.js
+++ streaming-dot.js
@@ -16,54 +16,44 @@
   Object.assign(exports, {
     version: "1.0.0",
     templateSettings: {
-      // evaluate:    /\{\{([\s\S]+?(\}?)+)\}\}/g,
+      evaluate:    /\{\{(([^\}]+|\\.)+)\}\}/g,
       interpolate: /\{\{=([^\}]+)\}\}/g,
       conditional: /\{\{\?\s*([^\}]*)\}\}/g,
-      // iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
       varname: "it",
-      parse: true,
-      node: false
-    },
-    template: undefined
+    }
   });
 
-  var startend = { start: "'); yield Promise.resolve(", end: "); yield Promise.resolve('"};
-  
   function unescape(code) {
     return code.replace(/\\('|\\)/g, "$1").replace(/[\r\t\n]/g, " ");
   }
 
-  exports.template = function(tmpl, c, def) {
+  exports.compile = function(tmpl, c, def) {
     c = Object.assign({}, exports.templateSettings, c);
-    var sid = 0, indv;
 
-    tmpl = 
-      `const g = (function* () {
-        yield Promise.resolve('`
-         + tmpl.replace(/'|\\/g, "\\$&")
+    tmpl = "var P=Promise.resolve.bind(Promise);var g=function* () {yield P('"
+        + tmpl
+            .replace(/'|\\/g, "\\$&")
             .replace(c.interpolate, function(m, code) {
-              return startend.start + unescape(code) + startend.end;
+              return "');yield P(" + unescape(code) + ");yield P('";
             })
             .replace(c.conditional, function(m, code) {
-              if (code) { // if
-                return "'); yield Promise.resolve(" + unescape(code) + ").then(v => {if(v){ return Promise.resolve('"
-              } else { // "endif"
-                return "');}else{return Promise.resolve('');}}); yield Promise.resolve('";
+              if (code) { // {{?<something>}} === if
+                return "');yield P(" + unescape(code) + ").then(v=>v&&P('"
+              } else { // {{?}} === "endif"
+                return "'));yield P('";
               }
             })
-         + `');
-      })();`;
+            .replace(c.evaluate, function(m, code) {
+              return "');" + unescape(code) + ";yield P('";
+            })
+         + "');}();";
 
     if(c.node) {
-      
       tmpl += 
-        `const emitter = new EventEmitter();
-        let p = Promise.resolve();
+        `var emitter = new EventEmitter();
+        var p = P();
         for(let v of g) {
-          p = 
-            p
-              .then(_ => v)
-              .then(v => emitter.emit('data', v)); 
+          p = p.then(_ => v).then(v => v && emitter.emit('data', v));
         }
         p.then(_ => emitter.emit('end'));
         return emitter`;
@@ -71,41 +61,18 @@
       tmpl +=
         `return new ReadableStream({
           pull: ctr => {
-            const v = g.next();
-            v.value.then(data => {
-              if (v.done) {
-                return ctr.close();
-              }
-              ctr.enqueue(data);
+            var v = g.next();
+            v.value && v.value.then(data => {
+              if (v.done) return ctr.close();
+              data && ctr.enqueue(data);
             });
             return v.value;
           }
         });`;
     }
 
-      // .replace(c.conditional || skip, function(m, elsecase, code) {
-      // 	return elsecase ?
-      // 		(code ? "';}else if(" + unescape(code) + "){out+='" : "';}else{out+='") :
-      // 		(code ? "';if(" + unescape(code) + "){out+='" : "';}out+='");
-      // })
-      // .replace(c.iterate || skip, function(m, iterate, vname, iname) {
-      // 	if (!iterate) return "';} } out+='";
-      // 	sid+=1; indv=iname || "i"+sid; iterate=unescape(iterate);
-      // 	return "';var arr"+sid+"="+iterate+";if(arr"+sid+"){var "+vname+","+indv+"=-1,l"+sid+"=arr"+sid+".length-1;while("+indv+"<l"+sid+"){"
-      // 		+vname+"=arr"+sid+"["+indv+"+=1];out+='";
-      // })
-      // .replace(c.evaluate || skip, function(m, code) {
-      // 	return "';" + unescape(code) + "w.write('";
-      // })
-      // + "');return out;")
-      // .replace(/\n/g, "\\n").replace(/\t/g, '\\t').replace(/\r/g, "\\r")
-      // .replace(/(\s|;|\}|^|\{)out\+='';/g, '$1').replace(/\+''/g, "");
-      //.replace(/(\s|;|\}|^|\{)out\+=''\+/g,'$1out+=');
-
     try {
-      if (!c.parse) {
-        return tmpl;
-      }
+      if (c.noEval) return tmpl;
       if (c.node) {
         const f = new Function(c.varname, 'EventEmitter', tmpl);
         return it => f(it, EventEmitter);
