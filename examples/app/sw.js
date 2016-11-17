58d7e13353f5203a56ab12b4f7e69cfc9730e8a0
Surma <surma@surma.link>
2016-11-17 21:00:41 +0000

    Adjust ServiceWorker for polyfill demo

--- examples/app/sw.js
+++ examples/app/sw.js
@@ -20,17 +20,21 @@ function timeoutPromise(t) {
   );
 }
 
-self.onfetch = event => event.respondWith(
-  caches.match('/index.dot.html')
-    .then(response => response.text())
-    .then(body => {
-      const template = doT.compile(body);
-      const response = template({
-        header: caches.match('/header.partial.html').then(r => r.body),
-        footer: caches.match('/footer.partial.html').then(r => r.body),
-        location: timeoutPromise(2000).then(_ => 'in a service worker'),
-        version: doT.version
-      });
-      return new Response(response, {headers: {'Content-Type': 'text/html'}});
-    })
-);
+self.onfetch = event => {
+  event.parsedUrl = new URL(event.request.url);
+  if (event.parsedUrl.pathname !== '/') return event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match('/index.dot.html')
+      .then(response => response.text())
+      .then(body => {
+        const template = doT.compile(body);
+        const response = template({
+          header: caches.match('/header.partial.html').then(r => r.body),
+          footer: caches.match('/footer.partial.html').then(r => r.body),
+          location: timeoutPromise(2000).then(_ => 'in a service worker'),
+          version: doT.version
+        });
+        return new Response(response, {headers: {'Content-Type': 'text/html'}});
+      })
+  );
+};
