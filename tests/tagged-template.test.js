7c48e9f7894138471511dd05f3f7d73b5e0b031c
Surma <surma@surma.dev>
2020-07-31 12:50:48 +0100

    Some more small tests

--- tests/tagged-template.test.js
+++ tests/tagged-template.test.js
@@ -12,15 +12,40 @@ export async function collect(stream) {
   }
 }
 
+function streamFromIterable(it) {
+  return new ReadableStream({
+    start(controller) {
+      for (const v of it) {
+        controller.enqueue(v);
+      }
+      controller.close();
+    },
+  });
+}
+
 function decodeArrayOfBuffers(arr) {
   const dec = new TextDecoder();
   return arr.map((v) => dec.decode(v, { stream: true })).join("");
 }
 
 describe("tagged-template dot", function () {
-  it("concatenates strings and values", async function () {
+  it("concatenates strings", async function () {
     const stream = dot`A${"B"}C`;
     const values = await collect(stream);
     expect(decodeArrayOfBuffers(values)).to.equal("ABC");
+  });
+
+  it("can handle streams of strings", async function () {
+    const otherStream = streamFromIterable(["1", "2", "3"]);
+    const stream = dot`A${otherStream}B`;
+    const values = await collect(stream);
+    expect(decodeArrayOfBuffers(values)).to.equal("A123B");
+  });
+
+  it("can handle binary streams", async function () {
+    const otherStream = streamFromIterable([new Uint8Array(["65"]).buffer]);
+    const stream = dot`1${otherStream}2`;
+    const values = await collect(stream);
+    expect(decodeArrayOfBuffers(values)).to.equal("1A2");
   });
 });
