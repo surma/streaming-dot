f9eec68b6585f8e683a566fb86614e940ce5eeae
Surma <surma@surma.dev>
2020-08-01 23:41:13 +0100

    Use uvu for tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
-import { promises as fsp } from "fs";
-
 import { terser } from "rollup-plugin-terser";
-import resolve from "@rollup/plugin-node-resolve";
 
 import simpleTS from "./rollup-plugins/simple-ts.js";
 
@@ -18,26 +15,6 @@ export default [
       terser({
         mangle: true,
       }),
-    ],
-  },
-  // Tests
-  {
-    input: "tests/runner.js",
-    output: {
-      dir: ".test-build",
-      format: "esm",
-    },
-    plugins: [
-      resolve(),
-      {
-        async buildStart() {
-          this.emitFile({
-            type: "asset",
-            fileName: "index.html",
-            source: await fsp.readFile("tests/index.html"),
-          });
-        },
-      },
     ],
   },
 ];
